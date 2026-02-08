44c30314963ce406dbfdf9daef3ef34f0592d229
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-08 12:22:09 +0300

    fix: update import path for db module in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { type Request, type Response } from "express";
-import { db } from "./db/index.js"
+import { db } from "./db/index"
 import { sql } from "drizzle-orm";
 import * as dotenv from "dotenv";
 
