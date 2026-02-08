d74053fbc56496e05017cd0672a9a8a1d8d5b0c2
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-08 12:08:41 +0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { type Request, type Response } from "express";
+import { db } from "./db/index.js"
+import { sql } from "drizzle-orm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Server-ku wuxuu ka kiciyey: http://localhost:${PORT}`);
+});
