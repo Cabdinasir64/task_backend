79b814b39475f128b85d91e9d22b77e0b29501b1
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-08 17:12:21 +0300

    feat: integrate schema into drizzle initialization for database connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as dotenv from "dotenv";
+import * as schema from "./schema"; 
 
 dotenv.config();
 
@@ -9,4 +10,4 @@ if (!process.env.DATABASE_URL) {
 }
 
 const client = postgres(process.env.DATABASE_URL);
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
