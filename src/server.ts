ee31ff362c56580646abc07ded950f8819f79a8d
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-14 19:14:45 +0300

    update waxaan ku soo darey logger si xogta guud u arki karno

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
 import taskRoutes from "./routes/task.routes.js";
+import logger from "./logger.js";
+
 import cors from "cors";
 
 dotenv.config();
@@ -10,18 +12,30 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(cors({
-  origin: [
-    "http://localhost:55756", 
-    "http://127.0.0.1:53035",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-  ],
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"]
-}));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:55756",
+      "http://127.0.0.1:53035",
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  }),
+);
 
 app.use("/api/tasks", taskRoutes);
 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  logger.error("Unhandled Exception:", err);
+  res.status(500).json({ error: "Something went wrong internally" });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  logger.info(`Server is running at http://localhost:${PORT}`);
 });
